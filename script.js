4ebe68eff4a91971028262a2da6fe85375c15c26
Helena Johansson <helena@klarr.se>
2022-10-20 13:50:55 +0200

    added functions

--- script.js
+++ script.js
@@ -10,6 +10,26 @@ const calculateWealthBtn = document.getElementById("calculate-wealth");
 let data = [];
 
 // get random user from API
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+
+  const data = await res.json();
+  //console.log(data);
+
+  const user = data.results[0];
+  // vi vil ha första item i arrayen vi får tillbaka som result
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    // vi får förnamn och efternamn på vår user
+    money: Math.floor(Math.random() * 1000000),
+  };
+
+  //console.log(newUser);
+  addData(newUser);
+}
+
+//getRandomUser();
 
 function doubleMoney() {
   // alla users property money ska dubblas
@@ -26,13 +46,35 @@ function showMillionaires() {
 }
 
 function calculateWealth() {
-  // räkna ihop allas pengar, alltså totala förnögenheten
+  // räkna ihop allas pengar, alltså totala förmögenheten
   // den här är svårast och använder en lite mer komplicerad array metod
 }
 
 // add new user to data array
+function addData(obj) {
+  data.push(obj);
+  // använder push för att lägga till objektet på den tomma arrayen
+
+  // uppdatera DOM
+  updateDOM();
+}
 
 // update the DOM
+function updateDOM(providedData = data) {
+  // cleara main div
+  main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+
+  // loopa igenom vår data array
+  providedData.forEach((person) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${person.name}</strong> ${formatMoney(
+      person.money
+    )}`;
+
+    main.appendChild(element);
+  });
+}
 
 // format money
 // https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
@@ -42,3 +84,4 @@ function formatMoney(number) {
 
 // event listeners
 // här lägger ni eventlisteners till alla knappar i menyn
+addUserBtn.addEventListener("click", getRandomUser);
